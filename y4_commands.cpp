f247b7221e45ed1aac2659ce8dc48890a8944c4b
Yuval Dolev <yuvaldolev2@gmail.com>
2021-03-27 00:44:37 +0300

    Added more bindings and improved visual line mode selection visualization

--- y4_commands.cpp
+++ y4_commands.cpp
@@ -5,3 +5,16 @@ CUSTOM_COMMAND_SIG(y4_interactive_switch_buffer_other_panel) {
 CUSTOM_COMMAND_SIG(y4_interactive_open_or_new_other_panel) {
     change_active_panel_send_command(app, interactive_open_or_new);
 }
+
+CUSTOM_COMMAND_SIG(y4_search_for_definition__project_wide_other_panel) {
+    change_active_panel_send_command(app, f4_search_for_definition__project_wide);
+}
+
+CUSTOM_COMMAND_SIG(y4_search_for_definition__current_file_other_panel) {
+    change_active_panel_send_command(app, f4_search_for_definition__current_file);
+}
+
+CUSTOM_COMMAND_SIG(y4_find_matching_file_other_panel) {
+    view_buffer_other_panel(app);
+    casey_find_matching_file(app);
+}
--- y4_commands.h
+++ y4_commands.h
@@ -9,4 +9,13 @@ CUSTOM_DOC("Switch buffer in the other panel");
 CUSTOM_COMMAND_SIG(y4_interactive_open_or_new_other_panel)
 CUSTOM_DOC("Open an existing file or create a new one in the other panel");
 
+CUSTOM_COMMAND_SIG(y4_search_for_definition__project_wide_other_panel)
+CUSTOM_DOC("Search for a definition throughout the whole project in the other panel");
+
+CUSTOM_COMMAND_SIG(y4_search_for_definition__current_file_other_panel)
+CUSTOM_DOC("Search for a definition throughout the current file in the other panel");
+
+CUSTOM_COMMAND_SIG(y4_find_matching_file_other_panel)
+CUSTOM_DOC("Find the matching file and open it in the other panel");
+
 #endif //Y4_COMMANDS_H
