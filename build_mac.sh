06628cb17b97c707f8959a81a444d485d98ebcf4
Yuval Dolev <yuvaldolev2@gmail.com>
2021-03-28 13:42:58 +0300

    Improvements to the macOS build script, making it work from any directory, and some bug fixes in 4coder-vimmish

--- build_mac.sh
+++ build_mac.sh
@@ -2,9 +2,15 @@
 
 set -e
 
-../bin/buildsuper_x64-mac.sh 4coder_yuval.cpp
-# ../bin/buildsuper_x64-mac.sh 4coder_fleury/4coder_fleury.cpp
-# ../bin/buildsuper_x64-mac.sh 4coder-vimmish/4coder_sandvich_maker.cpp
+SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
+Y4_DIR="$(dirname "$SCRIPT_PATH")"
+CUSTOM_DIR="$(dirname "$Y4_DIR")"
+FCODER_ROOT_DIR="$(dirname "$CUSTOM_DIR")"
 
-cp custom_4coder.so ../../custom_4coder.so
-cp -r custom_4coder.so.dSYM ../../custom_4coder.so.dSYM
+pushd "$FCODER_ROOT_DIR" &>/dev/null
+
+"$CUSTOM_DIR/bin/buildsuper_x64-mac.sh" "$Y4_DIR/4coder_yuval.cpp"
+# "$CUSTOM_DIR/bin/buildsuper_x64-mac.sh" "$Y4_DIR/4coder_fleury/4coder_fleury.cpp"
+# "$CUSTOM_DIR/bin/buildsuper_x64-mac.sh" "$Y4_DIR/4coder-vimmish/4coder_sandvich_maker.cpp"
+
+popd &>/dev/null
